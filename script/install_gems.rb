c76bf62026432ee0e9f62a5189ca23c34666cd9d
Hiroshi Nakamura <nahi@ruby-lang.org>
2011-03-02 06:49:14 +0900

    Columns alignment for readability.

--- script/install_gems.rb
+++ script/install_gems.rb
@@ -10,7 +10,6 @@ CSV.parse(File.read(CLIENTS)) do |row|
   name, gem, one_nine, repo = row.map { |e| e.strip }
   next if RUBY_VERSION >= "1.9" and one_nine == 'no'
   if gem == 'yes'
-    p name
-    #system "#{ruby} -S gem install #{name} --user-install --no-ri --no-rdoc"
+    system "#{ruby} -S gem install #{name} --user-install --no-ri --no-rdoc"
   end
 end
