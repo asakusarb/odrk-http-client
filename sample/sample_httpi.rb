9858467712fd025a30f0aeaceba1dd5a8d81c675
rubiii <me@rubiii.com>
2012-10-15 21:01:48 +0200

    simplified the httpi sample

--- sample/sample_httpi.rb
+++ sample/sample_httpi.rb
@@ -2,9 +2,6 @@ require 'httpi'
 
 require File.expand_path('sample_setting', File.dirname(__FILE__))
 
-HTTPI.adapter = :curb
-p HTTPI.get(HTTPI::Request.new(@url)).body.size
-HTTPI.adapter = :httpclient
-p HTTPI.get(HTTPI::Request.new(@url)).body.size
-HTTPI.adapter = :net_http
-p HTTPI.get(HTTPI::Request.new(@url)).body.size
+p HTTPI.get(@url, :curb).body.size
+p HTTPI.get(@url, :httpclient).body.size
+p HTTPI.get(@url, :net_http).body.size
